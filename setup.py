e719a60c3936b573438f81367bcb8ec34aac91e1
Rukchad Wongprayoon <contact@biomooping.tk>
2021-12-31 12:40:34 +0700

    why tf readthedocs dont allow _ in domain name >:(

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="ez_aiohttp",
-    version="0.0.2",
+    version="0.0.4",
     author="Rukchad Wongprayoon",
     author_email="contact@biomooping.tk",
     description="Make aiohttp easier",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://ez_aiohttp.rukchadisa.live",
+    url="https://ez-aiohttp.rukchadisa.live/en/latest/",
     packages=["ez_rq"],
     package_dir={"ez_rq": "src/ez_rq"},
     install_requires=open("requirements.txt").readlines(),
